9291b0511242bb7af6cb1677829f04380e06ad81
AbhinayBandaru <abhinay.b@live.in>
2016-04-21 13:07:02 +0200

    Update ad345test.py

--- ad345test.py
+++ ad345test.py
@@ -1,10 +1,29 @@
 import pyb
 from ad345 import AD345
+
+blue = pyb.LED(1)
+switch = pyb.Switch()
+
 spi = pyb.SPI(X, SPI.MASTER)
 spi.init(pyb.SPI.MASTER, baudrate=2000000, polarity=1, phase=0)
 
 ad = AD345(spi)
+
 print("SENSOR ID: %s" % (hex(ad.read_id())))
 
 while True:
-    print("ACCELEROMETER Readings: %s %s %s" % % % (ad.xyz()))  
+    
+    pyb.wfi()
+    
+    if switch():
+        pyb.delay(200)
+        blue.on()
+        log = open('/sd/log.csv', 'w')
+        while not switch():
+            t = pyb.millis()
+            x, y, z = ad.xyz()
+            log.write('{},{},{},{}\n'.format(t,x,y,z))
+        log.close()
+        blue.off()
+        pyb.delay(200)
+    
